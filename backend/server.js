15258cadaa0ffd97b3546129dd6640f16552ee21
Dushyant <mdkumar1405@gmail.com>
2025-04-22 00:23:42 +0530

    major frontend works

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { Server } from 'socket.io';
 const app=express();
 const roomId='EYYfy';
+let clientId=8989;
+let dict=[];
 app.use(express.static('public'));
 
 // app.get('/homepage',(req,res)=>{
@@ -16,35 +18,63 @@ const io=new Server(httpServer,{
 app.get('/',(req,res)=>{
     res.send("hi");
 })
-//proposed json structure
-// state:{
-//     player:{
-//         clientId:
-//         Name:
-//         vel:{x,y}
-//         x:
-//         y:
-//         gender:
-//          
-//     }
-// }
+
+const state={
+    'EYYfy':{
+        players:[
+          
+        ]
+    }
+};
+console.log(state[roomId]);
 io.on('connection',socket=>{
-    console.log("hi "+socket.id);
+    
 
-    socket.on("AddMember",()=>{
+    socket.on("addMember",(data)=>{
+        // console.log(socket.id);
+        if(data===undefined)return;
+        const {x,y,vel}=getPlayerInfo();
         socket.join(roomId);
-        const {x,y}=randomPosition();
-        socket.emit("newMember",socket)
-            
+        socket.emit("playerInfo",{clientId,x,y,vel});
+        socket.emit("allMembers",getPlayersData(roomId));//to sender
 
-        //for all members in the room broadcast the user details for now keep only male charcaters for everyone ..so it shd contain x,y,vel,
-        //only when one user clicks arrow button update their velocity else let it be constant
-        //event for clickdown and clickup broadcast this instead of x,y coordinates each client with that client.id will have their velocity updated..
+        state[roomId].players.push({
+            clientId:clientId,
+            x:x,
+            y:y,
+            vel:{x:0,y:0}
+        })
+        socket.to(roomId).emit("newMember",[clientId,x,y,vel]);// to all already present in the server except the sender
+        
+        clientId++; 
+        //randomPosition()//random coordinates on phaser window;
+        
+        
+
+       
+        
+        
 
 
     })
+    socket.on("playerMovement",(data)=>{
+        console.log(data);
+        socket.to(roomId).emit("otherPlayerMovement",data);
+    })
     
 })
+function getPlayerInfo(){
+    const {x,y,vel}= {x:Math.floor(400*(Math.random())),y:Math.floor(200*(Math.random())),vel:{x:0,y:0}};
+    return {x,y,vel};
+}
+function getAllMembers(){
+    const room=io.sockets.clients(roomId);
+}
+function getPlayersData(roomId){
+    const room =io.sockets.adapter.rooms.get(roomId);
+    const players={};
+    if(room)return state[roomId].players;
+}
 io.on('updateVelocity',data=>{
     console.log("this is the player instance"+data);
 })
